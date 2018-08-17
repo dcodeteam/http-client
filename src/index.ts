4f69e71ca6e219d3c8e098a27ff3c0af9252b442
Umidbek Karimov <uma.karimov@gmail.com>
2018-08-17 15:43:33 +0500

    feat(index): Cherry pick each module to export.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export * from "./utils/HttpErrorUtils";
-export * from "./http-client/HttpClient";
-export * from "./http-status/HttpStatus";
+export {
+  createHttpClientError,
+  isHttpClientError,
+  isHttpClientTimeoutError,
+  isHttpClientResponseError,
+  getHttpClientErrorStatus,
+  HTTP_ERROR_TIMEOUT_CODE
+} from "./utils/HttpErrorUtils";
+
+export { HttpClient, HttpClientOptions } from "./http-client/HttpClient";
+
+export { HttpStatus } from "./http-status/HttpStatus";
