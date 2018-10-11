29437c253475fdf8a2139a339cb1ec8514c967fc
Umidbek Karimov <uma.karimov@gmail.com>
2018-10-11 16:49:52 +0500

    chore: Use `babel@7` to transpile test code.

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,22 @@
 "use strict";
 
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsConfig: "./tsconfig.test.json",
+      babelConfig: {
+        presets: [["@babel/preset-env", { targets: { node: true } }]]
+      }
+    }
+  },
+
   testEnvironment: "node",
   setupFiles: ["<rootDir>/config/jest.js"],
-  transform: {
-    ".ts": "ts-jest"
-  },
+  transform: { ".ts": "ts-jest" },
   testMatch: ["**/src/**/*.spec.ts"],
   moduleFileExtensions: ["ts", "js"],
   collectCoverageFrom: ["src/**/*.ts", "!src/**/__tests__/**/*.ts"],
   coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
+    global: { branches: 95, functions: 95, lines: 95, statements: 95 }
   }
 };
