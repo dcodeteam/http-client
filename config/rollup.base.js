230b983108d3cfa13e33d1d389856066018d6554
Umidbek Karimov <uma.karimov@gmail.com>
2018-10-11 18:56:13 +0500

    chore: Build for `ES2015`.

--- config/rollup.base.js
+++ config/rollup.base.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const cleaner = require("rollup-plugin-cleaner");
 const replace = require("rollup-plugin-replace");
 const prettier = require("rollup-plugin-prettier");
 const typescript = require("rollup-plugin-typescript2");
@@ -8,7 +7,10 @@ const typescript = require("rollup-plugin-typescript2");
 module.exports = function createRollupConfig({ target }) {
   return {
     input: "./src/index.ts",
-    output: { format: target, file: `./${target}/index.js` },
+    output: {
+      file: `./http-client.${target}.js`,
+      format: target.startsWith("es") ? "es" : target
+    },
     external(id) {
       switch (id) {
         case "axios":
@@ -20,15 +22,19 @@ module.exports = function createRollupConfig({ target }) {
       }
     },
     plugins: [
-      cleaner({ targets: [`./${target}/`] }),
-
       typescript({
         clean: true,
-        useTsconfigDeclarationDir: true,
-        tsconfig: `./tsconfig.${target}.json`
+        tsconfig: `./tsconfig.build.json`,
+        tsconfigOverride: {
+          compilerOptions: {
+            module: "es2015",
+            target: target === "es2015" ? "es2015" : "es5"
+          }
+        }
       }),
 
       target === "es" && replace({ values: { rxjs: "rxjs/_esm5" } }),
+      target === "es2015" && replace({ values: { rxjs: "rxjs/_esm2015" } }),
 
       prettier({ parser: "babylon" })
     ].filter(Boolean)
