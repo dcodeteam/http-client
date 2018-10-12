03270130f28765743497771822a782da13e960c5
Umidbek Karimov <uma.karimov@gmail.com>
2018-10-12 13:18:04 +0500

    chore: Use `babel@7` to create bundle.

---
+++ config/rollup.config.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const babel = require("rollup-plugin-babel");
+const prettier = require("rollup-plugin-prettier");
+const nodeResolve = require("rollup-plugin-node-resolve");
+
+const pkg = require("../package");
+
+module.exports = [
+  createConfig("es"),
+  createConfig("cjs"),
+  createConfig("es2015")
+];
+
+function createConfig(target) {
+  const externals = [
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.peerDependencies)
+  ];
+
+  return {
+    input: "./src/index.ts",
+
+    output: {
+      file: `./http-client.${target}.js`,
+      format: target === "cjs" ? "cjs" : "es"
+    },
+
+    external(id) {
+      return (
+        externals.includes(id) ||
+        id.startsWith("rxjs") ||
+        id.startsWith("@babel/runtime")
+      );
+    },
+
+    plugins: [
+      nodeResolve({ extensions: [".ts"] }),
+
+      babel({
+        babelrc: false,
+        runtimeHelpers: true,
+        extensions: [".ts"],
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              modules: false,
+              forceAllTransforms: target !== "es2015",
+              targets: { esmodules: target === "es2015" }
+            }
+          ],
+          "@babel/preset-typescript"
+        ],
+        plugins: [
+          ["@babel/plugin-transform-runtime"],
+
+          target === "es" && [
+            "module-resolver",
+            {
+              alias: {
+                rxjs: "rxjs/_esm5",
+                "rxjs/operators": "rxjs/_esm5/operators"
+              }
+            }
+          ],
+          target === "es2015" && [
+            "module-resolver",
+            {
+              alias: {
+                rxjs: "rxjs/_esm2015",
+                "rxjs/operators": "rxjs/_esm2015/operators"
+              }
+            }
+          ]
+        ].filter(Boolean)
+      }),
+
+      prettier({ parser: "babylon" })
+    ]
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-export { HttpClient, HttpClientOptions } from "./http-client/HttpClient";
-
-export { HttpStatus } from "./http-status/HttpStatus";
-
-export { HttpClientError } from "./interfaces/HttpClientError";
-export { HttpClientResponse } from "./interfaces/HttpClientResponse";
-
-export {
-  HttpClientRequestConfig,
-  HttpClientFetchRequestConfig,
-  HttpClientUpdateRequestConfig
-} from "./interfaces/HttpClientRequestConfig";
-
-export { HttpClientRequestMethod } from "./interfaces/HttpClientRequestMethod";
-
-export {
-  createHttpClientError,
-  isHttpClientError,
-  isHttpClientTimeoutError,
-  isHttpClientResponseError,
-  getHttpClientErrorStatus,
-  HTTP_ERROR_TIMEOUT_CODE
-} from "./utils/HttpErrorUtils";
+export * from "./utils/HttpErrorUtils";
+export * from "./http-client/HttpClient";
+export * from "./http-status/HttpStatus";
+export * from "./interfaces/HttpClientError";
+export * from "./interfaces/HttpClientResponse";
+export * from "./interfaces/HttpClientRequestConfig";
+export * from "./interfaces/HttpClientRequestMethod";
